c92a31c5f3359e9cd1ffab2ea1970c60510431b9
Rameck-Junior <gadzisorameckjunior7@gmail.com>
2026-02-10 18:34:18 +0200

    Old project

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+
+#Pseudocode:
+# Request user to enter their name
+# Store the name
+# Display the name
+# Request user to enter their age
+# Store the age
+# Display the age
+# Display "Hello, World!"
+
+
+print("Enter your name: ")
+name = input()
+
+print("Your name is: " + name)
+
+print("Enter your age: ")
+age = int(input())
+
+print("Your age is: " + str(age))
+
+print("Hello, World!")
